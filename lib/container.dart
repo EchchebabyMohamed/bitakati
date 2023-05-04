3161e086fefd33c74ce36ac360d4e4b8d6a9cf72
Mohamed Ech-chebaby <mohamedchababi87@gmail.com>
2023-05-04 23:19:14 +0100

    premiere commit

---
+++ lib/container.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class ContainerC extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  const ContainerC({
+    super.key,
+    required this.icon,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 50),
+      child: ListTile(
+        leading: Icon(
+          icon,
+          color: Colors.cyan[700],
+        ),
+        title: Text(
+          text,
+          style: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'container.dart';
+
+void main() {
+  runApp(const Mywidget());
+}
+
+class Mywidget extends StatelessWidget {
+  const Mywidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.cyan[700],
+          body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('images/IMG_2788.JPG'),
+                ),
+                Text(
+                  'Mohamed',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  'developpeur',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(
+                  width: 200,
+                  height: 20,
+                  child: Divider(
+                    color: Color.fromARGB(255, 59, 225, 247),
+                  ),
+                ),
+                ContainerC(
+                  icon: Icons.phone,
+                  text: "022154698754",
+                ),
+                ContainerC(
+                  icon: Icons.email,
+                  text: "@gmail.com",
+                ),
+                ContainerC(
+                  icon: Icons.person,
+                  text: "Mohammed Ech",
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
